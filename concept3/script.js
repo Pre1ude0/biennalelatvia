08e256e73e116fc57d276865e19d075e090f20cf
Pre1ude0 <larss.jakobsons@icloud.com>
2024-12-26 12:36:13 +0200

    Initial commit

---
+++ concept3/script.js
@@ -0,0 +1,23 @@
+let backToTopButton = document.getElementById("btt");
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+  backToTopButton.classList.remove("down");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let startTime = Math.random() * 100;
+  let background = document.getElementsByClassName("bg")[0];
+
+  background.style.animationDelay = `-${startTime}s`;
+
+  document.addEventListener("scroll", function () {
+    if (window.scrollY < 100) {
+      backToTopButton.classList.add("down");
+    } else {
+      backToTopButton.classList.remove("down");
+    }
+  });
+});
+
+window.scrollToTop = scrollToTop;
