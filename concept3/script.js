1ca49bb7e56a0084fab71da0f9e1ca80536a3cb7
Pre1ude0 <larss.jakobsons@icloud.com>
2024-12-31 17:03:05 +0200

    add languages

--- concept3/script.js
+++ concept3/script.js
@@ -2,6 +2,8 @@ let backToTopButton = document.getElementById("btt");
 let content = document.getElementsByClassName("content")[0];
 const yOffset = -50;
 
+let lang = "en";
+
 function setSize() {
   if (window.innerWidth > 768) {
     content.style.width = `${530 * Math.floor(window.innerWidth / 530)}px`;
@@ -13,32 +15,65 @@ function scrollToTop() {
   backToTopButton.classList.remove("down");
 }
 
-function scrollToItem(item) {
-  if (item === "statement") {
-    let y =
-      document.getElementById("statementId").getBoundingClientRect().top +
-      window.scrollY +
-      yOffset;
-    scrollTo({ top: y, behavior: "smooth", inline: "start", block: "start" });
-  } else if (item === "team") {
-    let y =
-      document.getElementById("teamId").getBoundingClientRect().top +
-      window.scrollY +
-      yOffset;
-    scrollTo({ top: y, behavior: "smooth", inline: "start", block: "start" });
-  } else if (item === "events") {
-    let y =
-      document.getElementById("eventsId").getBoundingClientRect().top +
-      window.scrollY +
-      yOffset;
-    scrollTo({ top: y, behavior: "smooth", inline: "start", block: "start" });
-  } else if (item === "press") {
-    let y =
-      document.getElementById("pressId").getBoundingClientRect().top +
-      window.scrollY +
-      yOffset;
-    scrollTo({ top: y, behavior: "smooth", inline: "start", block: "start" });
+function setLanguage(language) {
+  console.log(language);
+  if (language === "en") {
+    document.body.classList.remove("lang-it");
+    document.body.classList.remove("lang-lv");
+    document.body.classList.add("lang-en");
+    lang = "en";
+  } else if (language === "it") {
+    document.body.classList.add("lang-it");
+    document.body.classList.remove("lang-lv");
+    document.body.classList.remove("lang-en");
+    lang = "it";
+  } else if (language === "lv") {
+    document.body.classList.remove("lang-it");
+    document.body.classList.add("lang-lv");
+    document.body.classList.remove("lang-en");
+    lang = "lv";
+  } else {
+    document.body.classList.remove("lang-it");
+    document.body.classList.remove("lang-lv");
+    document.body.classList.add("lang-en");
+    lang = "en";
   }
+}
+
+function scrollToItem(item) {
+  // if (item === "statement") {
+  //   let y =
+  //     document.getElementById("statementId").getBoundingClientRect().top +
+  //     window.scrollY +
+  //     yOffset;
+  //   scrollTo({ top: y, behavior: "smooth", inline: "start", block: "start" });
+  // } else if (item === "team") {
+  //   let y =
+  //     document.getElementById("teamId").getBoundingClientRect().top +
+  //     window.scrollY +
+  //     yOffset;
+  //   scrollTo({ top: y, behavior: "smooth", inline: "start", block: "start" });
+  // } else if (item === "events") {
+  //   let y =
+  //     document.getElementById("eventsId").getBoundingClientRect().top +
+  //     window.scrollY +
+  //     yOffset;
+  //   scrollTo({ top: y, behavior: "smooth", inline: "start", block: "start" });
+  // } else if (item === "press") {
+  //   let y =
+  //     document.getElementById("pressId").getBoundingClientRect().top +
+  //     window.scrollY +
+  //     yOffset;
+  //   scrollTo({ top: y, behavior: "smooth", inline: "start", block: "start" });
+  // }
+
+  console.log(`${item}Id-${lang}`);
+  let y =
+    document.getElementById(`${item}Id-${lang}`).getBoundingClientRect().top +
+    window.scrollY +
+    yOffset;
+
+  scrollTo({ top: y, behavior: "smooth", inline: "start", block: "start" });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -57,7 +92,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  window.addEventListener("hashchange", function () {
+    if (window.location.hash) {
+      setLanguage(window.location.hash.substring(1));
+    }
+  });
+
   setSize();
+
+  setLanguage(window.location.hash.substring(1));
+  console.log(window.location.hash.substring(1));
 
   window.addEventListener("resize", setSize);
 });
