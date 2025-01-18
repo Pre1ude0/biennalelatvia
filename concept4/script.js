7aa7dce0f4551830d182624d626cab479d227a2b
Pre1ude0 <larss.jakobsons@icloud.com>
2025-01-18 17:21:41 +0200

    new style

---
+++ concept4/script.js
@@ -0,0 +1,98 @@
+let backToTopButton = document.getElementById("btt");
+let content = document.getElementsByClassName("content")[0];
+const yOffset = -50;
+
+let lang = "en";
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+  backToTopButton.classList.remove("down");
+}
+
+function setLanguage(language) {
+  if (language === "en") {
+    document.body.classList.remove("lang-it");
+    document.body.classList.remove("lang-lv");
+    document.body.classList.add("lang-en");
+    lang = "en";
+  } else if (language === "it") {
+    document.body.classList.add("lang-it");
+    document.body.classList.remove("lang-lv");
+    document.body.classList.remove("lang-en");
+    lang = "it";
+  } else if (language === "lv") {
+    document.body.classList.remove("lang-it");
+    document.body.classList.add("lang-lv");
+    document.body.classList.remove("lang-en");
+    lang = "lv";
+  } else {
+    document.body.classList.remove("lang-it");
+    document.body.classList.remove("lang-lv");
+    document.body.classList.add("lang-en");
+    lang = "en";
+  }
+}
+
+function scrollToItem(item) {
+  // if (item === "statement") {
+  //   let y =
+  //     document.getElementById("statementId").getBoundingClientRect().top +
+  //     window.scrollY +
+  //     yOffset;
+  //   scrollTo({ top: y, behavior: "smooth", inline: "start", block: "start" });
+  // } else if (item === "team") {
+  //   let y =
+  //     document.getElementById("teamId").getBoundingClientRect().top +
+  //     window.scrollY +
+  //     yOffset;
+  //   scrollTo({ top: y, behavior: "smooth", inline: "start", block: "start" });
+  // } else if (item === "events") {
+  //   let y =
+  //     document.getElementById("eventsId").getBoundingClientRect().top +
+  //     window.scrollY +
+  //     yOffset;
+  //   scrollTo({ top: y, behavior: "smooth", inline: "start", block: "start" });
+  // } else if (item === "press") {
+  //   let y =
+  //     document.getElementById("pressId").getBoundingClientRect().top +
+  //     window.scrollY +
+  //     yOffset;
+  //   scrollTo({ top: y, behavior: "smooth", inline: "start", block: "start" });
+  // }
+
+  console.log(`${item}Id-${lang}`);
+  let y =
+    document.getElementById(`${item}Id-${lang}`).getBoundingClientRect().top +
+    window.scrollY +
+    yOffset;
+
+  scrollTo({ top: y, behavior: "smooth", inline: "start", block: "start" });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let startTime = Math.random() * 100;
+  let background = document.getElementsByClassName("bg")[0];
+
+  background.style.animationDelay = `-${startTime}s`;
+
+  document.addEventListener("scroll", function () {
+    if (window.innerWidth < 768) {
+      if (window.scrollY < 100) {
+        backToTopButton.classList.add("down");
+      } else {
+        backToTopButton.classList.remove("down");
+      }
+    }
+  });
+
+  window.addEventListener("hashchange", function () {
+    if (window.location.hash) {
+      setLanguage(window.location.hash.substring(1));
+    }
+  });
+
+  setLanguage(window.location.hash.substring(1));
+});
+
+window.scrollToTop = scrollToTop;
+window.scrollToItem = scrollToItem;
